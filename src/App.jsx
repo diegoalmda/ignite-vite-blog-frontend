9db8f266ec1bef1816b3117c444156a52308cfc1
Diego Almeida <diegoalmda@gmail.com>
2022-06-29 13:05:54 -0300

    Sidebar style

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+
+import styles from './App.module.css';
+import './global.css';
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Almeida"
+            content="Novo post para o blog."
+          />
+          <Post
+            author="Outro usuário"
+            content="Novo post mais legal do que o primeiro para o blog."
+          />
+        </main>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
