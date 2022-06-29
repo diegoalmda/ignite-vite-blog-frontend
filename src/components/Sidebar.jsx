fd56d68ec6237b33bba73807a901854cf77fd1f3
Diego Almeida <diegoalmda@gmail.com>
2022-06-29 14:37:15 -0300

    Post component styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';  
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +10,16 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1506606401543-2e73709cebb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/diegoalmda.png" />
         <strong>Diego Almeida</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
