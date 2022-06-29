0297ddec74e11731b98df24dcb68a9568f11031f
Diego Almeida <diegoalmda@gmail.com>
2022-06-29 15:36:18 -0300

    Created avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
@@ -7,7 +8,7 @@ export function Post({ author, content }) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/diegoalmda.png" />
+          <Avatar src="https://github.com/diegoalmda.png" />
           <div className={styles.authorInfo}>
             <strong>Diego Almeida</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';  
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -10,7 +11,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1506606401543-2e73709cebb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/diegoalmda.png" />
+        <Avatar src="https://github.com/diegoalmda.png" />
         <strong>Diego Almeida</strong>
         <span>Web Developer</span>
       </div>
